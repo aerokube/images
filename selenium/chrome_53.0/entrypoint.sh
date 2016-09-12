e3d1419c4a6f50afd51d0aed92493f1a15840b3e
Alexander Andryashin <aandryashin@yandex-team.ru>
2016-09-12 22:25:12 +0300

    Minor fixes
    Change-Id: Ied8ed2ea50aea32bae204c16de7a21d6e7df1a3f

--- selenium/chrome_53.0/entrypoint.sh
+++ selenium/chrome_53.0/entrypoint.sh
@@ -2,7 +2,7 @@
 
 NODE_PORT=${NODE_PORT:-4444}
 CHROMEDRIVER_PATH="/usr/bin/chromedriver "
-CHROMEDRIVER_ARGS="--port=$NODE_PORT --whitelisted-ips=""  --url-base=wd/hub --verbose"
+CHROMEDRIVER_ARGS="--port=$NODE_PORT --whitelisted-ips="" --verbose"
 
 echo "Starting Chromedriver on port $NODE_PORT..."
 xvfb-run -a -s '-screen 0 1280x1600x24 -noreset' $CHROMEDRIVER_PATH $CHROMEDRIVER_ARGS
