1dd072dc5d0fbb03b7a16c51c635b0b4b61b39c0
Ivan Krutov <vania-pooh@vania-pooh.com>
2017-03-25 16:04:14 +0300

    Cleanup fixes

--- selenium/build-dev.sh
+++ selenium/build-dev.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
+set -o verbose
 
 if [ -z $1 -o -z $2 ]; then
     echo 'Usage: build-dev.sh {firefox/official|firefox/ubuntuzilla|chrome|opera/presto|opera/blink} <browser_version> [<cleanup={true|false}>]'
@@ -15,7 +16,7 @@ if [ "$cleanup" == "false" ]; then
     tag=$tag"_full"
 fi
 pushd "$browser"
-echo "Creating image $tag..."
+echo "Creating image $tag with cleanup=$cleanup..."
 docker build --build-arg VERSION="$version" --build-arg CLEANUP="$cleanup" -t "$tag" .
 popd
 exit 0
--- selenium/build.sh
+++ selenium/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
+set -o verbose
 
 download_selenium() {
     selenium_version=$1
