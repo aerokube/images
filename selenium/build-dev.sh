ce2cb17ff939c9477f934d9c8c02f726b2bf90bb
Ivan Krutov <vania-pooh@vania-pooh.com>
2017-03-26 11:48:34 +0300

    Using Java-based container in build-dev.sh for Firefox if needed

--- selenium/build-dev.sh
+++ selenium/build-dev.sh
@@ -2,7 +2,7 @@
 set -e
 
 if [ -z "$1" -o -z "$2" ]; then
-    echo 'Usage: build-dev.sh {firefox/official|firefox/ubuntuzilla|chrome|opera/presto|opera/blink} <browser_version> [<cleanup={true|false}>]'
+    echo 'Usage: build-dev.sh {firefox/official|firefox/ubuntuzilla|chrome|opera/presto|opera/blink} <browser_version> [<cleanup={true|false}>] [<requires_java={true|false}>]'
     exit 1
 fi
 set -x
@@ -10,12 +10,24 @@ set -x
 browser=$1
 version=$2
 cleanup=${3:-"false"}
+requires_java=${4:-"false"}
 browser_name=$(echo "$browser" | sed -e 's/\(..*\)\/..*/\1/g')
 tag="selenoid/dev:"$browser_name"_"$version
 if [ "$cleanup" == "false" ]; then
     tag=$tag"_full"
 fi
-pushd "$browser"
+dir_name="/tmp/$(uuidgen | sed -e 's|-||g')"
+mkdir -p "$dir_name"
+if [ "$browser" == "firefox/ubuntuzilla" ]; then
+    requires_java_value=""
+    if [ "$requires_java" == "true" ]; then
+        requires_java_value="_java"
+    fi
+    cat "$browser/Dockerfile.tmpl" | sed -e "s|@@REQUIRES_JAVA@@|$requires_java_value|g" > "$dir_name/Dockerfile"
+else
+    cp "$browser/Dockerfile" "$dir_name"
+fi
+pushd "$dir_name"
 echo "Creating image $tag with cleanup=$cleanup..."
 docker build --build-arg VERSION="$version" --build-arg CLEANUP="$cleanup" -t "$tag" .
 popd
