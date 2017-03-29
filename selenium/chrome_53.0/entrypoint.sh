f8a84a08165eb6102abfb1ffa328bbaa460a26d9
Ivan Krutov <vania-pooh@vania-pooh.com>
2017-03-29 19:16:49 +0300

    Removed old containers

--- selenium/chrome_53.0/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-NODE_PORT=${NODE_PORT:-4444}
-CHROMEDRIVER_PATH="/usr/bin/chromedriver "
-CHROMEDRIVER_ARGS="--port=$NODE_PORT --whitelisted-ips="" --verbose"
-
-echo "Starting Chromedriver on port $NODE_PORT..."
-xvfb-run -a -s '-screen 0 1280x1600x24 -noreset' $CHROMEDRIVER_PATH $CHROMEDRIVER_ARGS
--- selenium/firefox_48.0/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-NODE_PORT=${NODE_PORT:-4444}
-GECKODRIVER_PATH="/usr/bin/geckodriver"
-GECKODRIVER_ARGS="--host :: --port $NODE_PORT --log debug"
-
-echo "Starting Geckodriver on port $NODE_PORT..."
-xvfb-run -a -s '-screen 0 1280x1600x24 -noreset' $GECKODRIVER_PATH $GECKODRIVER_ARGS
