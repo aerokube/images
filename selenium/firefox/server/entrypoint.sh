91d8a1160f698990c192f96ecafe6fbb82e9f15f
Ivan Krutov <vania-pooh@vania-pooh.com>
2017-04-03 18:19:17 +0300

    Better entrypoint.sh

--- selenium/firefox/server/entrypoint.sh
+++ selenium/firefox/server/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 SCREEN_RESOLUTION=${SCREEN_RESOLUTION:-"1920x1080x24"}
-/usr/bin/xvfb-run -a -s "-screen 0 $SCREEN_RESOLUTION -noreset" /usr/bin/java -Xmx256m -Djava.security.egd=file:/dev/./urandom -Dwebdriver.gecko.driver=/usr/lib/selenium/geckodriver -jar /usr/share/selenium/selenium-server-standalone.jar -port 4444 -timeout 60 -browserTimeout 120
+/usr/bin/xvfb-run -a -s "-screen 0 $SCREEN_RESOLUTION -noreset" /usr/bin/java -Xmx256m -Djava.security.egd=file:/dev/./urandom -jar /usr/share/selenium/selenium-server-standalone.jar -port 4444 -timeout 60 -browserTimeout 120
