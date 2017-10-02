3677ca832beccecead561bd9233fbe8017f6f4a8
Ivan Krutov <vania-pooh@vania-pooh.com>
2017-10-02 16:43:49 +0300

    Added Android image prototype data

---
+++ selenium/android/entrypoint.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+CHROMEDRIVER_PORT=9515
+BOOTSTRAP_PORT=4725
+EMULATOR=emulator-5554
+CHROME_ARGS=""
+TIMEOUT=${TIMEOUT:-"60"}
+SKIN=${SKIN:-"WXGA800"}
+
+if [ -n "$DENSITY" ]; then
+    echo "hw.lcd.density=$DENSITY" >> /root/.android/avd/android6.0-1.avd/config.ini
+fi
+
+Xorg :0 &
+sleep 1
+ANDROID_AVD_HOME=/root/.android/avd DISPLAY=:0 /opt/android-sdk-linux/emulator/emulator -no-window -no-audio -no-boot-anim -no-jni -avd android6.0-1 -sdcard /sdcard.img -skin "$SKIN" -skindir /opt/android-sdk-linux/platforms/android-23/skins/ -verbose -gpu mesa -qemu -enable-kvm &
+sleep 30
+
+#if [ -n "$INSTALL_APP" ]; then
+#	for i in 1 2 3 4 5; do adb install /chrome.apk && break || sleep 15 && echo "retrying install app"; done
+#fi
+
+sleep 5
+#if [ -z "$ADD_APP" ]; then
+#	CHROME_ARGS="--chromedriver-port $CHROMEDRIVER_PORT --app-pkg \"com.android.chrome\" --app-activity \"com.google.android.apps.chrome.Main\" --no-reset"
+#fi
+/opt/node_modules/.bin/appium -a 0.0.0.0 -p $PORT -bp $BOOTSTRAP_PORT -U $EMULATOR --platform-name Android --device-name android --log-timestamp --log-no-colors --default-capabilities "{\"newCommandTimeout\": \"$TIMEOUT\"}" $CHROME_ARGS
