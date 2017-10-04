2e81655de9d3b492b9a49914782197c7fb50cebf
Ivan Krutov <vania-pooh@vania-pooh.com>
2017-10-04 17:52:43 +0300

    Added -enable-file-upload to gecko+selenoid Firefox images (related to #15)

--- selenium/firefox/gecko+selenoid/entrypoint.sh
+++ selenium/firefox/gecko+selenoid/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 SCREEN_RESOLUTION=${SCREEN_RESOLUTION:-"1920x1080x24"}
-/usr/bin/xvfb-run -a -s "-screen 0 $SCREEN_RESOLUTION -noreset" /usr/bin/selenoid -conf /etc/selenoid/browsers.json -disable-docker -timeout 1h -retry-count 3
+/usr/bin/xvfb-run -a -s "-screen 0 $SCREEN_RESOLUTION -noreset" /usr/bin/selenoid -conf /etc/selenoid/browsers.json -disable-docker -timeout 1h -retry-count 3 -enable-file-upload
