fe6e6cca43b9e2f06c53fcb6e9d1105609d6a359
Ivan Krutov <vania-pooh@vania-pooh.com>
2017-11-02 20:10:39 +0300

    Added rebuild-old.sh script

---
+++ selenium/base/base_2/rebuild-old.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+if [ -z "$1" ]; then
+    echo 'Usage: rebuild-old.sh <image_ref>'
+    exit 1
+fi
+set -x
+
+image=$1
+
+dir_name="/tmp/$(uuidgen | sed -e 's|-||g')"
+mkdir -p "$dir_name"
+
+cat "Dockerfile" | sed -e "s|selenoid/base:1.0|$image|g" > "$dir_name/Dockerfile"
+pushd "$dir_name"
+echo "Adding fonts and encondings to $ref..."
+docker build -t "$ref" .
+popd
+rm -Rf "$dir_name"
+exit 0
