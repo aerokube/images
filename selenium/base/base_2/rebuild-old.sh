fa59cc282fb80fb373e34fdd412c08031638d16d
Ivan Krutov <vania-pooh@vania-pooh.com>
2017-11-02 20:16:18 +0300

    Fixed typo

--- selenium/base/base_2/rebuild-old.sh
+++ selenium/base/base_2/rebuild-old.sh
@@ -15,8 +15,8 @@ mkdir -p "$dir_name"
 
 cat "Dockerfile" | sed -e "s|selenoid/base:1.0|$image|g" > "$dir_name/Dockerfile"
 pushd "$dir_name"
-echo "Adding fonts and encondings to $ref..."
-docker build -t "$ref" .
+echo "Adding fonts and encondings to $image..."
+docker build -t "$image" .
 popd
 rm -Rf "$dir_name"
 exit 0
