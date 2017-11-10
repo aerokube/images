8a61999a1f2a54b689201fd15c2e56a079642202
Ivan Krutov <vania-pooh@vania-pooh.com>
2017-11-10 19:27:51 +0300

    Capturing driver logs for Selenoid+Geckodriver images (related to #17)

--- selenium/vnc/firefox/gecko+selenoid/entrypoint.sh
+++ selenium/vnc/firefox/gecko+selenoid/entrypoint.sh
@@ -2,4 +2,4 @@
 SCREEN_RESOLUTION=${SCREEN_RESOLUTION:-"1920x1080x24"}
 DISPLAY=99
 XAUTHORITY=/tmp/xvfb.auth x11vnc -display ":$DISPLAY" -passwd selenoid -shared -forever -loop500 -rfbport 5900 -rfbportv6 5900 &
-/usr/bin/xvfb-run -f /tmp/xvfb.auth -n "$DISPLAY" -s "-screen 0 $SCREEN_RESOLUTION -noreset -auth /tmp/xvfb.auth" /usr/bin/selenoid -conf /etc/selenoid/browsers.json -disable-docker -timeout 1h -retry-count 3 -enable-file-upload
+/usr/bin/xvfb-run -f /tmp/xvfb.auth -n "$DISPLAY" -s "-screen 0 $SCREEN_RESOLUTION -noreset -auth /tmp/xvfb.auth" /usr/bin/selenoid -conf /etc/selenoid/browsers.json -disable-docker -timeout 1h -retry-count 3 -enable-file-upload -capture-driver-logs
