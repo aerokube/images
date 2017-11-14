76ab73947eea6a4772574c9b32d39c7af4877db0
Anton Dzyk <anton.dzyk@yandex.ru>
2017-11-14 11:06:12 +0000

    new video image

--- selenium/video/entrypoint.sh
+++ selenium/video/entrypoint.sh
@@ -2,4 +2,15 @@
 VIDEO_SIZE=${VIDEO_SIZE:-"1920x1080"}
 DISPLAY=${DISPLAY:-"99"}
 FILE_NAME=${FILE_NAME:-"video.mp4"}
-exec ffmpeg -y -f x11grab -video_size "$VIDEO_SIZE" -i "browser:$DISPLAY" -codec:v libx264 -r 12 "/data/$FILE_NAME"
+for i in {0..50}
+do
+	nc -z browser 60${PORT}
+	if [ $? -ne 0 ]
+	then
+		echo 'wait...'
+		sleep 0.1
+	else	
+		break
+	fi
+done
+exec ffmpeg -y -f x11grab -video_size $VIDEO_SIZE -r 12 -i browser:$DISPLAY -codec:v libx264 "/data/$FILE_NAME"
