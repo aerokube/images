ca9d461a11e8ebd2b31b09f568e8a1eeb22eaa19
Ivan Krutov <vania-pooh@vania-pooh.com>
2017-11-17 18:47:02 +0300

    Updated Firefox automation script

--- selenium/automate_firefox.sh
+++ selenium/automate_firefox.sh
@@ -11,26 +11,45 @@ if [ -z "$1" -o -z "$2" -o -z "$3" -o -z "$4" ]; then
 fi
 set -x
 
-./build-dev.sh firefox/apt $browser_version false false $tag
-./build-dev.sh firefox/apt $browser_version true false $tag
-pushd firefox/gecko+selenoid
-../../build.sh gecko+selenoid $tag $selenoid_version selenoid/firefox:$tag $driver_version
+method=firefox/apt
+runner=gecko+selenoid
+./build-dev.sh $method $browser_version false false $tag
+./build-dev.sh $method $browser_version true false $tag
+pushd firefox/$runner
+../../build.sh $runner $tag $selenoid_version selenoid/firefox:$tag $driver_version
 popd
-docker rm -f selenium || true
-docker run -d --name selenium -p 4444:4444  selenoid/firefox:$tag
-tests_dir=../../selenoid-container-tests/
-if [ -d "$tests_dir" ]; then
-    pushd "$tests_dir"
-    mvn clean test -Dgrid.browser.version=$tag || true
+
+test_image(){
+    docker rm -f selenium || true
+    docker run -d --name selenium -p 4444:4444  selenoid/firefox:$1
+    tests_dir=../../selenoid-container-tests/
+    if [ -d "$tests_dir" ]; then
+        pushd "$tests_dir"
+        mvn clean test -Dgrid.browser.version=$1 || true
+        popd
+    else
+        echo "Skipping tests as $tests_dir does not exist."
+    fi
+}
+
+test_image "selenoid/firefox:"$tag
+read -p "Create VNC?" vnc
+if [ "$vnc" == "y" ]; then
+    pushd vnc/firefox/$runner
+    ../../../build-vnc.sh firefox $tag 
     popd
-else
-    echo "Skipping tests as $tests_dir does not exist."
+    test_image "selenoid/vnc_firefox:"$tag
 fi
+
 read -p "Push?" yn
 if [ "$yn" == "y" ]; then
-	docker push "selenoid/dev:firefox_"$tag
-	docker push "selenoid/dev:firefox_"$tag"_full"
+	docker push "selenoid/dev_firefox:"$tag
+	docker push "selenoid/dev_firefox_full:"$tag
 	docker push "selenoid/firefox:$tag"
     docker tag "selenoid/firefox:$tag" "selenoid/firefox:latest"
     docker push "selenoid/firefox:latest"
+    if [ "$vnc" == "y" ]; then
+        docker push "selenoid/vnc:firefox_"$tag
+        docker push "selenoid/vnc_firefox:"$tag
+    fi    
 fi
