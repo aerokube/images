cd27689154f2ca82684c3263d5142f7bc7f3324c
Ivan Krutov <vania-pooh@vania-pooh.com>
2017-11-20 07:02:15 +0300

    Updated automation script for Opera

--- selenium/automate_opera.sh
+++ selenium/automate_opera.sh
@@ -16,20 +16,38 @@ set -x
 pushd opera/blink
 ../../build.sh operadriver $browser_version $driver_version selenoid/opera:$tag
 popd
-docker rm -f selenium || true
-docker run -d --name selenium -p 4444:4444  selenoid/opera:$tag
-tests_dir=../../selenoid-container-tests/
-if [ -d "$tests_dir" ]; then
-    pushd "$tests_dir"
-    mvn clean test -Dgrid.connection.url="http://localhost:4444/" -Dgrid.browser.version=$tag -Dgrid.browser.name=opera || true
+
+test_image(){
+    docker rm -f selenium || true
+    docker run -d --name selenium -p 4444:4444  $1:$2
+    tests_dir=../../selenoid-container-tests/
+    if [ -d "$tests_dir" ]; then
+        pushd "$tests_dir"
+        mvn clean test -Dgrid.connection.url="http://localhost:4444/" -Dgrid.browser.name=opera -Dgrid.browser.version=$2 || true
+        popd
+    else
+        echo "Skipping tests as $tests_dir does not exist."
+    fi
+}
+
+test_image "selenoid/opera" $tag
+read -p "Create VNC?" vnc
+if [ "$vnc" == "y" ]; then
+    pushd vnc/opera/blink
+    ../../../build-vnc.sh opera $tag 
     popd
-else
-    echo "Skipping tests as $tests_dir does not exist."
+    test_image "selenoid/vnc_opera" $tag
 fi
+
 read -p "Push?" yn
 if [ "$yn" == "y" ]; then
-	docker push "selenoid/dev:opera_"$browser_version
+	docker push "selenoid/dev_opera:"$browser_version
+	docker push "selenoid/dev_opera_full:"$browser_version
 	docker push "selenoid/opera:$tag"
     docker tag "selenoid/opera:$tag" "selenoid/opera:latest"
     docker push "selenoid/opera:latest"
+    if [ "$vnc" == "y" ]; then
+        docker push "selenoid/vnc:opera_"$tag
+        docker push "selenoid/vnc_opera:"$tag
+    fi    
 fi
