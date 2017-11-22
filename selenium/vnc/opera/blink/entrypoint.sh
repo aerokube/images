4f68103b1f99bec7b5e8c143fc0f14d43f22a966
Ivan Krutov <vania-pooh@vania-pooh.com>
2017-11-22 06:51:44 +0300

    Sending x11vnc logs to file instead of stdout

--- selenium/vnc/opera/blink/entrypoint.sh
+++ selenium/vnc/opera/blink/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 SCREEN_RESOLUTION=${SCREEN_RESOLUTION:-"1920x1080x24"}
 DISPLAY=99
-x11vnc -display ":$DISPLAY" -passwd selenoid -shared -forever -loop500 -rfbport 5900 -rfbportv6 5900 &
+x11vnc -display ":$DISPLAY" -passwd selenoid -shared -forever -loop500 -rfbport 5900 -rfbportv6 5900 -logfile /var/log/x11vnc.log &
 /usr/bin/xvfb-run -l -n "$DISPLAY" -s "-ac -screen 0 $SCREEN_RESOLUTION -noreset -listen tcp" /usr/bin/operadriver --port=4444 --whitelisted-ips='' --verbose
