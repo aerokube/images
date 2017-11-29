b63185afc2ee55478c51e9a00d9315b2e67613ef
Anton Dzyk <adzyk@solit-clouds.ru>
2017-11-29 09:03:15 +0300

    FIX: wait tcp listen xvfb, add option FRAME_RATE

--- selenium/video/entrypoint.sh
+++ selenium/video/entrypoint.sh
@@ -2,9 +2,10 @@
 VIDEO_SIZE=${VIDEO_SIZE:-"1920x1080"}
 DISPLAY=${DISPLAY:-"99"}
 FILE_NAME=${FILE_NAME:-"video.mp4"}
+RATE=${FRAME_RATE:-"12"}
 for i in {0..50}
 do
-	nc -z browser 60${PORT}
+	nc -z browser 60${DISPLAY}
 	if [ $? -ne 0 ]
 	then
 		echo 'wait...'
@@ -13,4 +14,4 @@ do
 		break
 	fi
 done
-exec ffmpeg -y -f x11grab -video_size $VIDEO_SIZE -r 12 -i browser:$DISPLAY -codec:v libx264 "/data/$FILE_NAME"
+exec ffmpeg -y -f x11grab -video_size $VIDEO_SIZE -r ${RATE} -i browser:$DISPLAY -codec:v libx264 "/data/$FILE_NAME"
