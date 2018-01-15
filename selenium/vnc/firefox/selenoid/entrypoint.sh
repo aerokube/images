89de6519bf1c9b55f61b46805ab36f60cfa6a6b2
Ivan Krutov <vania-pooh@vania-pooh.com>
2018-01-15 17:34:38 +0300

    Fixed x11vnc.log path for latest Firefox versions

--- selenium/vnc/firefox/selenoid/entrypoint.sh
+++ selenium/vnc/firefox/selenoid/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 SCREEN_RESOLUTION=${SCREEN_RESOLUTION:-"1920x1080x24"}
 DISPLAY=99
-x11vnc -display ":$DISPLAY" -passwd selenoid -shared -forever -loop500 -rfbport 5900 -rfbportv6 5900 -logfile /var/log/x11vnc.log &
+x11vnc -display ":$DISPLAY" -passwd selenoid -shared -forever -loop500 -rfbport 5900 -rfbportv6 5900 -logfile /home/selenium/x11vnc.log &
 /usr/bin/xvfb-run -l -n "$DISPLAY" -s "-ac -screen 0 $SCREEN_RESOLUTION -noreset -listen tcp" /usr/bin/selenoid -conf /etc/selenoid/browsers.json -disable-docker -timeout 1h -enable-file-upload -capture-driver-logs
