7f02ee4338fc3ec8943339bd14a8b1b64a56b49f
Ivan Krutov <vania-pooh@aerokube.com>
2018-01-19 20:19:02 +0300

    Added ability to adjust browser container name for video recording

--- selenium/video/entrypoint.sh
+++ selenium/video/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 VIDEO_SIZE=${VIDEO_SIZE:-"1920x1080"}
+BROWSER_CONTAINER_NAME=${BROWSER_CONTAINER_NAME:-"browser"}
 DISPLAY=${DISPLAY:-"99"}
 FILE_NAME=${FILE_NAME:-"video.mp4"}
 RATE=${FRAME_RATE:-"12"}
@@ -14,4 +15,4 @@ do
 		break
 	fi
 done
-exec ffmpeg -y -f x11grab -video_size $VIDEO_SIZE -r ${RATE} -i browser:$DISPLAY -codec:v libx264 "/data/$FILE_NAME"
+exec ffmpeg -y -f x11grab -video_size $VIDEO_SIZE -r ${RATE} -i $BROWSER_CONTAINER_NAME:$DISPLAY -codec:v libx264 "/data/$FILE_NAME"
