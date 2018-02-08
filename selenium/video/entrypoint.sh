8403dccd662a8e04e079bdc219158a7b1a27074f
Ivan Krutov <vania-pooh@vania-pooh.com>
2018-02-08 11:32:27 +0300

    Migrated to xset in video container

--- selenium/video/entrypoint.sh
+++ selenium/video/entrypoint.sh
@@ -3,16 +3,15 @@ VIDEO_SIZE=${VIDEO_SIZE:-"1920x1080"}
 BROWSER_CONTAINER_NAME=${BROWSER_CONTAINER_NAME:-"browser"}
 DISPLAY=${DISPLAY:-"99"}
 FILE_NAME=${FILE_NAME:-"video.mp4"}
-RATE=${FRAME_RATE:-"12"}
-for i in {0..50}
-do
-	nc -z ${BROWSER_CONTAINER_NAME} 60${DISPLAY}
-	if [ $? -ne 0 ]
-	then
-		echo 'wait...'
+FRAME_RATE=${FRAME_RATE:-"12"}
+retcode=1
+echo 'Waiting for display to open...'
+until [ $retcode -eq 0 ]; do
+	xset -display ${BROWSER_CONTAINER_NAME}:${DISPLAY} b off > /dev/null 2>&1
+	retcode=$?
+	if [ $retcode -ne 0 ]; then
+		echo 'Sleeping before next attempt...'
 		sleep 0.1
-	else	
-		break
 	fi
 done
-exec ffmpeg -y -f x11grab -video_size $VIDEO_SIZE -r ${RATE} -i $BROWSER_CONTAINER_NAME:$DISPLAY -codec:v libx264 "/data/$FILE_NAME"
+exec ffmpeg -y -f x11grab -video_size $VIDEO_SIZE -r ${FRAME_RATE} -i $BROWSER_CONTAINER_NAME:$DISPLAY -codec:v libx264 "/data/$FILE_NAME"
