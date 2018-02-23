7fbbf7b9abf6c5129422163bcf74b3fd2e67f33d
Boni Garcia <bgarcia@gsyc.es>
2018-02-23 11:39:40 +0100

    Install Fluxbox window manager in VNC images

--- selenium/vnc/chrome/entrypoint.sh
+++ selenium/vnc/chrome/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 SCREEN_RESOLUTION=${SCREEN_RESOLUTION:-"1920x1080x24"}
 DISPLAY=99
+fluxbox -display :$DISPLAY &
 x11vnc -display ":$DISPLAY" -passwd selenoid -shared -forever -loop500 -rfbport 5900 -rfbportv6 5900 -logfile /home/selenium/x11vnc.log &
 /usr/bin/xvfb-run -l -n "$DISPLAY" -s "-ac -screen 0 $SCREEN_RESOLUTION -noreset -listen tcp" /usr/bin/chromedriver --port=4444 --whitelisted-ips='' --verbose
--- selenium/vnc/firefox/selenoid/entrypoint.sh
+++ selenium/vnc/firefox/selenoid/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 SCREEN_RESOLUTION=${SCREEN_RESOLUTION:-"1920x1080x24"}
 DISPLAY=99
+fluxbox -display :$DISPLAY &
 x11vnc -display ":$DISPLAY" -passwd selenoid -shared -forever -loop500 -rfbport 5900 -rfbportv6 5900 -logfile /home/selenium/x11vnc.log &
 /usr/bin/xvfb-run -l -n "$DISPLAY" -s "-ac -screen 0 $SCREEN_RESOLUTION -noreset -listen tcp" /usr/bin/selenoid -conf /etc/selenoid/browsers.json -disable-docker -timeout 1h -enable-file-upload -capture-driver-logs
