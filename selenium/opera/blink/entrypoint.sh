bcf9845c96fd8bb217f9f886f9878fe3d73a5a2b
Boni Garcia <bgarcia@gsyc.es>
2018-02-26 15:23:05 +0100

    Install x11-utils (binary xdpyinfo, used for waiting xvfb display)

--- selenium/opera/blink/entrypoint.sh
+++ selenium/opera/blink/entrypoint.sh
@@ -4,16 +4,15 @@ DISPLAY=99
 /usr/bin/xvfb-run -l -n "$DISPLAY" -s "-ac -screen 0 $SCREEN_RESOLUTION -noreset -listen tcp" /usr/bin/operadriver --port=4444 --whitelisted-ips='' --verbose &
 XVFB_PID=$!
 
-for i in $(seq 1 10)
-do
-  xdpyinfo -display $DISPLAY >/dev/null 2>&1
-  if [ $? -eq 0 ]; then
-    break
+retcode=1
+until [ $retcode -eq 0 ]; do
+  xdpyinfo -display :$DISPLAY >/dev/null 2>&1
+  retcode=$?
+  if [ $retcode -ne 0 ]; then
+    echo Waiting xvfb...
+    sleep 0.1
   fi
-  echo Waiting xvfb...
-  sleep 0.5
 done
 
 fluxbox -display :$DISPLAY &
-
 wait $XVFB_PID
--- selenium/vnc/opera/blink/entrypoint.sh
+++ selenium/vnc/opera/blink/entrypoint.sh
@@ -1,22 +1,19 @@
 #!/bin/bash
 SCREEN_RESOLUTION=${SCREEN_RESOLUTION:-"1920x1080x24"}
 DISPLAY=99
-
 /usr/bin/xvfb-run -l -n "$DISPLAY" -s "-ac -screen 0 $SCREEN_RESOLUTION -noreset -listen tcp" /usr/bin/operadriver --port=4444 --whitelisted-ips='' --verbose &
 XVFB_PID=$!
 
-for i in $(seq 1 10)
-do
-  xdpyinfo -display $DISPLAY >/dev/null 2>&1
-  if [ $? -eq 0 ]; then
-    break
+retcode=1
+until [ $retcode -eq 0 ]; do
+  xdpyinfo -display :$DISPLAY >/dev/null 2>&1
+  retcode=$?
+  if [ $retcode -ne 0 ]; then
+    echo Waiting xvfb...
+    sleep 0.1
   fi
-  echo Waiting xvfb...
-  sleep 0.5
 done
 
 fluxbox -display :$DISPLAY &
-
 x11vnc -display ":$DISPLAY" -passwd selenoid -shared -forever -loop500 -rfbport 5900 -rfbportv6 5900 -logfile /home/selenium/x11vnc.log &
-
 wait $XVFB_PID
