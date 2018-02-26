c5f73c054a658ccfb43c29752e26e685d06f4151
Boni Garcia <bgarcia@gsyc.es>
2018-02-26 10:06:39 +0100

    Revert "Install Fluxbox window manager in VNC images"
    This reverts commit 7fbbf7b9abf6c5129422163bcf74b3fd2e67f33d.

--- selenium/vnc/opera/presto/entrypoint.sh
+++ selenium/vnc/opera/presto/entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 SCREEN_RESOLUTION=${SCREEN_RESOLUTION:-"1920x1080x24"}
 DISPLAY=99
-fluxbox -display :$DISPLAY &
 x11vnc -display ":$DISPLAY" -passwd selenoid -shared -forever -loop500 -rfbport 5900 -rfbportv6 5900 -logfile /var/log/x11vnc.log &
 /usr/bin/xvfb-run -l -n "$DISPLAY" -s "-ac -screen 0 $SCREEN_RESOLUTION -noreset -listen tcp" /usr/bin/java -Xmx256m -Djava.security.egd=file:/dev/./urandom -jar /usr/share/selenium/selenium-server-standalone.jar -port 4444 -browserTimeout 120
