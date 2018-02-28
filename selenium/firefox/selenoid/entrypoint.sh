25b26bddf04d5fd506e3cd2ca84585536e3e6b2b
Boni García <bgarcia@gsyc.es>
2018-02-28 12:10:40 +0100

    Install Fluxbox window manager in VNC images (fixes #30)
    Added Fluxbox to browser images

--- selenium/firefox/selenoid/entrypoint.sh
+++ selenium/firefox/selenoid/entrypoint.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 SCREEN_RESOLUTION=${SCREEN_RESOLUTION:-"1920x1080x24"}
 DISPLAY=99
-/usr/bin/xvfb-run -l -n "$DISPLAY" -s "-ac -screen 0 $SCREEN_RESOLUTION -noreset -listen tcp" /usr/bin/selenoid -conf /etc/selenoid/browsers.json -disable-docker -timeout 1h -enable-file-upload -capture-driver-logs
+/usr/bin/xvfb-run -l -n "$DISPLAY" -s "-ac -screen 0 $SCREEN_RESOLUTION -noreset -listen tcp" /usr/bin/selenoid -conf /etc/selenoid/browsers.json -disable-docker -timeout 1h -enable-file-upload -capture-driver-logs &
+XVFB_PID=$!
+
+retcode=1
+until [ $retcode -eq 0 ]; do
+  xdpyinfo -display :$DISPLAY >/dev/null 2>&1
+  retcode=$?
+  if [ $retcode -ne 0 ]; then
+    echo Waiting xvfb...
+    sleep 0.1
+  fi
+done
+
+fluxbox -display :$DISPLAY &
+wait $XVFB_PID
--- selenium/vnc/firefox/selenium/entrypoint.sh
+++ selenium/vnc/firefox/selenium/entrypoint.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 SCREEN_RESOLUTION=${SCREEN_RESOLUTION:-"1920x1080x24"}
 DISPLAY=99
+/usr/bin/xvfb-run -l -n "$DISPLAY" -s "-ac -screen 0 $SCREEN_RESOLUTION -noreset -listen tcp" /usr/bin/java -Xmx256m -Djava.security.egd=file:/dev/./urandom -jar /usr/share/selenium/selenium-server-standalone.jar -port 4444 -browserTimeout 120 &
+XVFB_PID=$!
+
+retcode=1
+until [ $retcode -eq 0 ]; do
+  xdpyinfo -display :$DISPLAY >/dev/null 2>&1
+  retcode=$?
+  if [ $retcode -ne 0 ]; then
+    echo Waiting xvfb...
+    sleep 0.1
+  fi
+done
+
+fluxbox -display :$DISPLAY &
 x11vnc -display ":$DISPLAY" -passwd selenoid -shared -forever -loop500 -rfbport 5900 -rfbportv6 5900 -logfile /var/log/x11vnc.log &
-/usr/bin/xvfb-run -l -n "$DISPLAY" -s "-ac -screen 0 $SCREEN_RESOLUTION -noreset -listen tcp" /usr/bin/java -Xmx256m -Djava.security.egd=file:/dev/./urandom -jar /usr/share/selenium/selenium-server-standalone.jar -port 4444 -browserTimeout 120
+wait $XVFB_PID
