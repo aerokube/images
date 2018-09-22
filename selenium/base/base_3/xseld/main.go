cbeef43e3443a5d8985d782fd43b67cf8eefac73
Alexander Andryashin <aandryashin@gmail.com>
2018-09-22 19:17:27 +0300

    Added paste

--- selenium/base/base_3/xseld/main.go
+++ selenium/base/base_3/xseld/main.go
@@ -10,13 +10,30 @@ import (
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		cmd := exec.Command("xsel")
-		stdout, err := cmd.StdoutPipe()
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
+		switch r.Method {
+		case http.MethodGet:
+			stdout, err := cmd.StdoutPipe()
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			go io.Copy(w, stdout)
+		case http.MethodPost:
+			cmd.Args = append(cmd.Args, "-i")
+			stdin, err := cmd.StdinPipe()
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			go func() {
+				defer stdin.Close()
+				io.Copy(stdin, r.Body)
+			}()
+		default:
+			http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
 			return
 		}
-		go io.Copy(w, stdout)
-		err = cmd.Run()
+		err := cmd.Run()
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
