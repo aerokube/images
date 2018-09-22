72020a39e15aad4d119043c3bdc610af19245e15
Alexander Andryashin <aandryashin@gmail.com>
2018-09-22 15:51:56 +0300

    Clipboard selection daemon.

---
+++ selenium/base/base_3/xseld/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+	"os/exec"
+)
+
+func main() {
+	http.HandleFunc("/clipboard", func(w http.ResponseWriter, r *http.Request) {
+		cmd := exec.Command("xsel")
+		stdout, err := cmd.StdoutPipe()
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		go io.Copy(w, stdout)
+		err = cmd.Run()
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	})
+	log.Fatal(http.ListenAndServe(":9090", nil))
+}
