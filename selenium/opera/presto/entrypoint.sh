2d063badc400a5906475066283df890d69ef7e06
Ivan Krutov <vania-pooh@vania-pooh.com>
2018-12-02 16:08:06 +0300

    Exporting DISPLAY variable, turn off verbose by default, removed --host :: (fixes #53, fixes #71, fixes #73)

--- selenium/opera/presto/entrypoint.sh
+++ selenium/opera/presto/entrypoint.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 SCREEN_RESOLUTION=${SCREEN_RESOLUTION:-"1920x1080x24"}
 ENABLE_WINDOW_MANAGER=${ENABLE_WINDOW_MANAGER:-""}
-DISPLAY=99
+DISPLAY_NUM=99
+export DISPLAY=":$DISPLAY_NUM"
 
 clean() {
   if [ -n "$FILESERVER_PID" ]; then
     kill -TERM "$FILESERVER_PID"
   fi
+  if [ -n "$XSELD_PID" ]; then
+    kill -TERM "$XSELD_PID"
+  fi  
   if [ -n "$XVFB_PID" ]; then
     kill -TERM "$XVFB_PID"
   fi
@@ -23,12 +27,15 @@ trap clean SIGINT SIGTERM
 /usr/bin/fileserver &
 FILESERVER_PID=$!
 
-/usr/bin/xvfb-run -l -n "$DISPLAY" -s "-ac -screen 0 $SCREEN_RESOLUTION -noreset -listen tcp" /usr/bin/java -Xmx256m -Djava.security.egd=file:/dev/./urandom -jar /usr/share/selenium/selenium-server-standalone.jar -port 4444 -browserTimeout 120 &
+DISPLAY="$DISPLAY" /usr/bin/xseld &
+XSELD_PID=$!
+
+/usr/bin/xvfb-run -l -n "$DISPLAY_NUM" -s "-ac -screen 0 $SCREEN_RESOLUTION -noreset -listen tcp" /usr/bin/java -Xmx256m -Djava.security.egd=file:/dev/./urandom -jar /usr/share/selenium/selenium-server-standalone.jar -port 4444 -browserTimeout 120 &
 XVFB_PID=$!
 
 retcode=1
 until [ $retcode -eq 0 ]; do
-  xdpyinfo -display :$DISPLAY >/dev/null 2>&1
+  xdpyinfo -display "$DISPLAY" >/dev/null 2>&1
   retcode=$?
   if [ $retcode -ne 0 ]; then
     echo Waiting xvfb...
@@ -37,12 +44,12 @@ until [ $retcode -eq 0 ]; do
 done
 
 if [ -n "$ENABLE_WINDOW_MANAGER" ]; then
-    fluxbox -display :$DISPLAY &
+    fluxbox -display "$DISPLAY" &
     FLUXBOX_PID=$!
 fi
 
 if [ "$ENABLE_VNC" == "true" ]; then
-    x11vnc -display ":$DISPLAY" -passwd selenoid -shared -forever -loop500 -rfbport 5900 -rfbportv6 5900 -logfile /home/selenium/x11vnc.log &
+    x11vnc -display "$DISPLAY" -passwd selenoid -shared -forever -loop500 -rfbport 5900 -rfbportv6 5900 -logfile /home/selenium/x11vnc.log &
     X11VNC_PID=$!
 fi
 wait
