9cca65f7e2cf0f670e3d6227417854adfdd71364
Ivan Krutov <vania-pooh@vania-pooh.com>
2019-06-17 11:37:57 +0300

    Added ability to pass libx264 preset via variables

--- selenium/video/entrypoint.sh
+++ selenium/video/entrypoint.sh
@@ -5,6 +5,10 @@ DISPLAY=${DISPLAY:-"99"}
 FILE_NAME=${FILE_NAME:-"video.mp4"}
 FRAME_RATE=${FRAME_RATE:-"12"}
 CODEC=${CODEC:-"libx264"}
+PRESET=${PRESET:-""}
+if [ "$CODEC" == "libx264" -a -n "$PRESET" ]; then
+    PRESET="-preset $PRESET"
+fi
 retcode=1
 max_attempts=300
 attempts=0
@@ -18,4 +22,4 @@ until [ $retcode -eq 0 -o $attempts -eq $max_attempts ]; do
 	fi
 	attempts=$((attempts+1))
 done
-exec ffmpeg -y -f x11grab -video_size ${VIDEO_SIZE} -r ${FRAME_RATE} -i ${BROWSER_CONTAINER_NAME}:${DISPLAY} -codec:v ${CODEC} -pix_fmt yuv420p "/data/$FILE_NAME"
+exec ffmpeg -y -f x11grab -video_size ${VIDEO_SIZE} -r ${FRAME_RATE} -i ${BROWSER_CONTAINER_NAME}:${DISPLAY} -codec:v ${CODEC} ${PRESET} -pix_fmt yuv420p "/data/$FILE_NAME"
