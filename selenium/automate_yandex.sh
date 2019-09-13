85ac10f6dfad6864bccbe6195bb29488b3ac7f4a
Ivan Krutov <vania-pooh@vania-pooh.com>
2019-09-13 11:49:57 +0400

    Migrated yandex-browser images build files to publicly available driver

--- selenium/automate_yandex.sh
+++ selenium/automate_yandex.sh
@@ -5,7 +5,7 @@ driver_version=$2
 tag=$3
 
 if [ -z "$1" -o -z "$2" -o -z "$3" ]; then
-    echo 'Usage: automate_yandex.sh <browser_version|package_file> <operadriver_version> <tag_version>'
+    echo 'Usage: automate_yandex.sh <browser_version|package_file> <yandexdriver_version> <tag_version>'
     exit 1
 fi
 set -x
@@ -24,7 +24,7 @@ if [ "$method" == "yandex/apt" ]; then
     ./build-dev.sh $method $browser_version false
 fi
 pushd yandex
-../build.sh operadriver $browser_version $driver_version selenoid/yandex:$tag
+../build.sh yandexdriver $browser_version $driver_version selenoid/yandex-browser:$tag
 popd
 
 test_image(){
@@ -33,16 +33,14 @@ test_image(){
     tests_dir=../../selenoid-container-tests/
     if [ -d "$tests_dir" ]; then
         pushd "$tests_dir"
-        mvn clean test -Dgrid.connection.url="http://localhost:4445/" -Dgrid.browser.name=yandex -Dgrid.browser.version=$2 || true
+        mvn clean test -Dgrid.connection.url="http://localhost:4445/" -Dgrid.browser.name=yandex-browser -Dgrid.browser.version=$2 || true
         popd
     else
         echo "Skipping tests as $tests_dir does not exist."
     fi
 }
 
-test_image "selenoid/yandex" $tag
-docker tag "selenoid/yandex:$tag" "selenoid/vnc_yandex:$tag"
-docker tag "selenoid/yandex:$tag" "selenoid/vnc:yandex_$tag"
+test_image "selenoid/yandex-browser" $tag
 
 read -p "Push?" yn
 if [ "$yn" == "y" ]; then
@@ -50,9 +48,7 @@ if [ "$yn" == "y" ]; then
 	if [ "$method" == "yandex/apt" ]; then
 	    docker push "selenoid/dev_yandex_full:"$browser_version
     fi
-	docker push "selenoid/yandex:$tag"
-    docker tag "selenoid/yandex:$tag" "selenoid/yandex:latest"
-    docker push "selenoid/yandex:latest"
-    docker push "selenoid/vnc:yandex_"$tag
-    docker push "selenoid/vnc_yandex:"$tag
+	docker push "selenoid/yandex-browser:$tag"
+    docker tag "selenoid/yandex-browser:$tag" "selenoid/yandex-browser:latest"
+    docker push "selenoid/yandex-browser:latest"
 fi
