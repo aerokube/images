15ff2857a0f71977f708998f7f2e1fb2fe81446d
Ivan Krutov <vania-pooh@vania-pooh.com>
2020-06-25 12:42:28 +0300

    Added QUIET environment variable to Android images

--- selenium/android/entrypoint.sh
+++ selenium/android/entrypoint.sh
@@ -3,12 +3,19 @@ CHROMEDRIVER_PORT=9515
 BOOTSTRAP_PORT=4725
 EMULATOR=emulator-5554
 APPIUM_ARGS=${APPIUM_ARGS:-""}
+EMULATOR_ARGS=${EMULATOR_ARGS:-"-verbose"}
 PORT=${PORT:-"4444"}
 DISPLAY_NUM=99
 export DISPLAY=":$DISPLAY_NUM"
 SCREEN_RESOLUTION=${SCREEN_RESOLUTION:-"1920x1080x24"}
 SKIN=${SKIN:-"1080x1920"}
 STOP=""
+QUIET=${QUIET:-""}
+
+if [ -z "$QUIET" ]; then
+    APPIUM_ARGS="$APPIUM_ARGS --log-level error"
+fi
+
 
 clean() {
   STOP="yes"
@@ -31,7 +38,7 @@ clean() {
 
 trap clean SIGINT SIGTERM
 
-/usr/bin/xvfb-run -e /dev/stdout -l -n "$DISPLAY_NUM" -s "-ac -screen 0 $SCREEN_RESOLUTION -noreset -listen tcp" /usr/bin/fluxbox -display "$DISPLAY" -log /root/fluxbox.log 2>/dev/null &
+/usr/bin/xvfb-run -e /dev/stdout -l -n "$DISPLAY_NUM" -s "-ac -screen 0 $SCREEN_RESOLUTION -noreset -listen tcp" /usr/bin/fluxbox -display "$DISPLAY" -log /tmp/fluxbox.log 2>/dev/null &
 XVFB_PID=$!
 
 retcode=1
@@ -48,11 +55,11 @@ if [ -n "$STOP" ]; then exit 0; fi
 if [ "$ENABLE_VNC" != "true" -a "$ENABLE_VIDEO" != "true" ]; then
     EMULATOR_ARGS="-no-window"
 fi
-ANDROID_AVD_HOME=/root/.android/avd DISPLAY="$DISPLAY" /opt/android-sdk-linux/emulator/emulator ${EMULATOR_ARGS} -writable-system -no-boot-anim -no-audio -no-jni -avd @AVD_NAME@ -sdcard /sdcard.img -skin "$SKIN" -skindir /opt/android-sdk-linux/platforms/@PLATFORM@/skins/ -verbose -gpu swiftshader_indirect -ranchu -qemu -enable-kvm &
+ANDROID_AVD_HOME=/root/.android/avd DISPLAY="$DISPLAY" /opt/android-sdk-linux/emulator/emulator ${EMULATOR_ARGS} -writable-system -no-boot-anim -no-audio -no-jni -avd @AVD_NAME@ -sdcard /sdcard.img -skin "$SKIN" -skindir /opt/android-sdk-linux/platforms/@PLATFORM@/skins/ ${EMULATOR_ARGS} -gpu swiftshader_indirect -ranchu -qemu -enable-kvm &
 EMULATOR_PID=$!
 
 if [ "$ENABLE_VNC" == "true" ]; then
-    x11vnc -display "$DISPLAY" -passwd selenoid -shared -forever -loop500 -rfbport 5900 -rfbportv6 5900 -logfile /root/x11vnc.log &
+    x11vnc -display "$DISPLAY" -passwd selenoid -shared -forever -loop500 -rfbport 5900 -rfbportv6 5900 -logfile /tmp/x11vnc.log &
     X11VNC_PID=$!
 fi
 
