87e800d756b84e950c9704834857b43534cbafd6
Ivan Krutov <vania-pooh@vania-pooh.com>
2020-08-13 19:01:02 +0300

    Installing pkger for building images

--- ci/build.sh
+++ ci/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 export GO111MODULE="on"
+go get github.com/markbates/pkger/cmd/pkger
 go get -u github.com/mitchellh/gox # cross compile
 go generate github.com/aerokube/selenoid-images
 GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -ldflags "-X github.com/aerokube/selenoid-images/cmd.buildStamp=`date -u '+%Y-%m-%d_%I:%M:%S%p'` -X github.com/aerokube/selenoid-images/cmd.gitRevision=`git describe --tags || git rev-parse HEAD` -s -w"
