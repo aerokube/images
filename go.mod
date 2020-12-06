88049787bfafa01c14d60c3c98d26d768bb38398
Alexander Andryashin <aandryashin@gmail.com>
2020-12-06 20:28:37 +0300

    Enabled sound with pulseaudio.

--- go.mod
+++ go.mod
@@ -5,7 +5,7 @@ go 1.15
 require (
 	github.com/aandryashin/matchers v0.0.0-20161126170413-435295ea180e
 	github.com/fatih/color v1.9.0 // indirect
-	github.com/markbates/pkger v0.17.0
+	github.com/markbates/pkger v0.17.1
 	github.com/mattn/go-colorable v0.1.7 // indirect
 	github.com/mattn/go-runewidth v0.0.9 // indirect
 	github.com/spf13/cobra v1.0.0
--- selenium/video/entrypoint.sh
+++ selenium/video/entrypoint.sh
@@ -14,6 +14,7 @@ HIDE_CURSOR=${HIDE_CURSOR:-""}
 if [ -n "$HIDE_CURSOR" ]; then
     INPUT_OPTIONS="$INPUT_OPTIONS -draw_mouse 0"
 fi
+
 retcode=1
 max_attempts=300
 attempts=0
@@ -27,4 +28,15 @@ until [ $retcode -eq 0 -o $attempts -eq $max_attempts ]; do
 	fi
 	attempts=$((attempts+1))
 done
-exec ffmpeg -y -f x11grab -video_size ${VIDEO_SIZE} -r ${FRAME_RATE} ${INPUT_OPTIONS} -i ${BROWSER_CONTAINER_NAME}:${DISPLAY} -codec:v ${CODEC} ${PRESET} -pix_fmt yuv420p "/data/$FILE_NAME"
+
+mkdir -p ~/.config/pulse
+
+echo -n 'gIvST5iz2S0J1+JlXC1lD3HWvg61vDTV1xbmiGxZnjB6E3psXsjWUVQS4SRrch6rygQgtpw7qmghDFTaekt8qWiCjGvB0LNzQbvhfs1SFYDMakmIXuoqYoWFqTJ+GOXYByxpgCMylMKwpOoANEDePUCj36nwGaJNTNSjL8WBv+Bf3rJXqWnJ/43a0hUhmBBt28Dhiz6Yqowa83Y4iDRNJbxih6rB1vRNDKqRr/J9XJV+dOlM0dI+K6Vf5Ag+2LGZ3rc5sPVqgHgKK0mcNcsn+yCmO+XLQHD1K+QgL8RITs7nNeF1ikYPVgEYnc0CGzHTMvFR7JLgwL2gTXulCdwPbg=='| base64 -d>~/.config/pulse/cookie
+
+export PULSE_SERVER=${BROWSER_CONTAINER_NAME}
+
+if pactl info >/dev/null 2>&1; then
+  exec ffmpeg -f pulse -i default -y -f x11grab -video_size ${VIDEO_SIZE} -r ${FRAME_RATE} ${INPUT_OPTIONS} -i ${BROWSER_CONTAINER_NAME}:${DISPLAY} -codec:v ${CODEC} ${PRESET} -pix_fmt yuv420p "/data/$FILE_NAME"
+else
+  exec ffmpeg -y -f x11grab -video_size ${VIDEO_SIZE} -r ${FRAME_RATE} ${INPUT_OPTIONS} -i ${BROWSER_CONTAINER_NAME}:${DISPLAY} -codec:v ${CODEC} ${PRESET} -pix_fmt yuv420p "/data/$FILE_NAME"
+fi
