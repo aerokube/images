4ee3b467b626aa252d8899efef0e29b1cb8355d6
Alexander Andryashin <aandryashin@gmail.com>
2021-02-10 13:33:22 +0300

    Permissions fixes.

--- static/edge/entrypoint.sh
+++ static/edge/entrypoint.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-source /usr/bin/permissions.sh
 SCREEN_RESOLUTION=${SCREEN_RESOLUTION:-"1920x1080x24"}
 DISPLAY_NUM=99
 export DISPLAY=":$DISPLAY_NUM"
@@ -16,6 +15,9 @@ clean() {
   fi
   if [ -n "$XSELD_PID" ]; then
     kill -TERM "$XSELD_PID"
+  fi
+  if [ -n "$PULSE_PID" ]; then
+    kill -TERM "$PULSE_PID"
   fi
   if [ -n "$XVFB_PID" ]; then
     kill -TERM "$XVFB_PID"
@@ -46,6 +48,12 @@ FILESERVER_PID=$!
 
 DISPLAY="$DISPLAY" /usr/bin/xseld &
 XSELD_PID=$!
+
+mkdir -p ~/pulse/.config/pulse
+echo -n 'gIvST5iz2S0J1+JlXC1lD3HWvg61vDTV1xbmiGxZnjB6E3psXsjWUVQS4SRrch6rygQgtpw7qmghDFTaekt8qWiCjGvB0LNzQbvhfs1SFYDMakmIXuoqYoWFqTJ+GOXYByxpgCMylMKwpOoANEDePUCj36nwGaJNTNSjL8WBv+Bf3rJXqWnJ/43a0hUhmBBt28Dhiz6Yqowa83Y4iDRNJbxih6rB1vRNDKqRr/J9XJV+dOlM0dI+K6Vf5Ag+2LGZ3rc5sPVqgHgKK0mcNcsn+yCmO+XLQHD1K+QgL8RITs7nNeF1ikYPVgEYnc0CGzHTMvFR7JLgwL2gTXulCdwPbg=='| base64 -d>~/pulse/.config/pulse/cookie
+HOME=$HOME/pulse pulseaudio --start --exit-idle-time=-1
+HOME=$HOME/pulse pactl load-module module-native-protocol-tcp
+PULSE_PID=$(ps --no-headers -C pulseaudio -o pid | sed -r 's/( )+//g')
 
 /usr/bin/xvfb-run -l -n "$DISPLAY_NUM" -s "-ac -screen 0 $SCREEN_RESOLUTION -noreset -listen tcp" /usr/bin/fluxbox -display "$DISPLAY" -log /dev/null 2>/dev/null &
 XVFB_PID=$!
